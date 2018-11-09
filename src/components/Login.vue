abed61df43ecd8549dff04c0d6584be4d49596a2
Jordan Skomer <mail@jordanskomer.com>
2018-11-09 08:12:39 -0500

    Install Electron

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <a @click="login('google')">Google Sign In</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component('login', {
+  methods: {
+    /**
+     * Communicates with the backend api to
+     * login the user using the following types
+     * google
+     * facebook
+     * default
+     *
+     * @param type - The type of login to auth with
+     */
+    login: function(type: string) {
+      console.log(type);
+    }
+  }
+})
+export default class Home extends Vue {}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import Axios from 'axios';
 
 Vue.config.productionTip = false;
+const environment = window.location.host === 'app.kohkane.com' ?
+  'production' : window.location.host === 'staging.kohane.com' ?
+  'staging' :
+  'development';
+const baseUrls = {
+  production: 'https://api.kohkane.com/v1',
+  staging: 'https://api.kohkane.com/staging',
+  development: 'http://0.0.0.0:3000',
+};
+
+Axios.defaults.baseURL = baseUrls[environment];
+Vue.prototype.$http = Axios;
 
 new Vue({
   router,
